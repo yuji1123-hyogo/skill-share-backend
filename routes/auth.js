42ee69d6636af21c4a2460e399bcdafb95994e8c
Yuji Yamaguchi <yuji1123200113@gmail.com>
2024-12-09 02:02:22 +0900

    Replace bcrypt with bcryptjs

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const router = require("express").Router()
 const User = require("../models/User")
-const bcrypt = require("bcrypt")
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 
