77adc46a4ebc244bba1a93fff6dccffa84a376c6
Yuji Yamaguchi <yuji1123200113@gmail.com>
2024-12-09 15:16:18 +0900

    allow front-end-origin

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const checkJWT = require("./midlewear/checkJWT")
 
 
 const corsOptions = {
-    origin: ["http://localhost:3001","http://localhost:3000", "https://skill-share-frontend.vercel.app"],  // 許可するオリジン
+    origin: ["http://localhost:3001","http://localhost:3000", "https://skill-share-frontend-3zcm.vercel.app/"],  // 許可するオリジン
     credentials: true                 // クッキーを許可
   };
 
